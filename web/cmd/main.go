2b472c4dba971914a1d3e97f8db1ce001bd04eb2
Erkin Djindjiev <erkin@guardyourdomain.com>
2024-02-29 10:14:53 -0500

    Updated listen host

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -11,7 +11,7 @@ var (
 	configFileFlag = flag.String("c", "logs-snare-web.yml", "path to config yaml file")
 	debugFileFlag  = flag.Bool("d", false, "debug")
 	resetDbFlag    = flag.Bool("r", false, "reset db")
-	listenHost     = flag.String("p", "http://0.0.0.0:8080", "listen host")
+	listenHost     = flag.String("p", "0.0.0.0:8080", "listen host")
 )
 
 func main() {
